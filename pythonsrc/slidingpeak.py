aff3c691fefbcc8fe25d6ea079c042d95631116c
yszhong <15yszhong@tongji.edu.cn>
2016-10-06 22:11:39 +0800

    basic comparison for PPT

--- pythonsrc/slidingpeak.py
+++ pythonsrc/slidingpeak.py
@@ -10,8 +10,10 @@ This program realizes an algorithm of anomaly detection.
 import numpy
 from sklearn import cluster
 import os
+import math
 import pandas
 import logging
+import pywt # need to install wavelet package from auto/pywavelets in anaconda
 
 def readfile():
 	# read anomaly groundtruth
@@ -60,27 +62,12 @@ def readallfile():
 	f.close()
 	return ts
 
-def slidingwindow(M, winsize):
+def slidingwindow(M, winsize): 
 	partts = []
 	for i in range(M.shape[1]):
 		part = M[:, i:i + winsize]
 		partts.append(part)
 	return partts	
-
-def fftdetect(mat):
-	freq = []
-	for i in range(mat.shape[0]):
-		f = numpy.fft.fft(mat[[i], :])
-		f = abs(f)
-		freq.append(f[0][1])
-	freq = numpy.array(freq)
-	label = numpy.zeros(len(freq))
-	i = 0
-	for num in freq:
-		if num > 3 * numpy.mean(freq):
-			label[i] = 1
-		i += 1
-	return label
 
 def clustering(mat):
 	ap = cluster.AffinityPropagation(damping = 0.75)
@@ -90,6 +77,32 @@ def clustering(mat):
 	label = numpy.zeros(len(lab))
 	for i in range(lab):
 		if -af[i, lab[i]] > numpy.linalg.norm(mat[lab[i]]):
+			label[i] = 1
+	return label
+
+def waveletdetect(mat):
+	wvlt = []
+	for row in mat:
+		#wvlt = pywt.dwt(row,'haar')
+		wdec = pywt.wavedec(row, "haar", level = int(math.log(len(row), 2)))
+		wvlt.append(wdec[0][0])
+	wvlt = numpy.array(wvlt)
+	return wvlt
+
+def fftdetect(mat): # fft and anomaly if more than mean
+	freq = []
+	for i in range(mat.shape[0]):
+		f = numpy.fft.fft(mat[[i], :])
+		f = abs(f[0])
+		freq.append(f[0])
+	freq = numpy.array(freq)
+	return freq
+
+def pauta(vec):
+	vec = numpy.array(vec)
+	label = numpy.zeros(len(vec))
+	for i in range(len(vec)):
+		if abs(vec[i] - numpy.mean(vec) / numpy.std(vec)) > 3:
 			label[i] = 1
 	return label
 
@@ -115,16 +128,33 @@ def slidingpeak(winsize):
 	partts = slidingwindow(ts, winsize)
 	lab = []
 	for i in range(ts.shape[1] - winsize):
-		dl = fftdetect(partts[i])
+		dl = waveletdetect(partts[i])
+		"""
+		if i == 0:
+			f=open("../result/tsequence1.txt","w")
+			for i in range(len(dl)):
+				f.write(str(dl[i])+"\n")
+			f.close()
+			print "done"
+		"""
+		dl = pauta(dl)
 		lab.append(dl)
 	lab = numpy.array(lab)
 	label = numpy.zeros(ts.shape[0])
 	for i in range(lab.shape[1]):
 		temp = lab[:, [i]]
-		if numpy.count_nonzero(temp) >= winsize - 1:
+		if numpy.count_nonzero(temp) >= winsize:
 			label[i] = 1
 	eval = evaluate(label, ground)
 	return eval
+
+def nowin():
+	ts, ground = readfile()
+	lab = waveletdetect(ts)
+	lab = numpy.array(lab)
+	eval = evaluate(lab, ground)
+	return eval
+
 
 def outputwindow(winsize):
 	ts = readallfile()
@@ -143,25 +173,27 @@ def outputwindow(winsize):
 		f.close()
 
 if __name__ == "__main__":
-	#"""
-	# set logging configure
+	# cut log size
 	logsize = os.path.getsize("../result/slidingpeak.log")
-	if logsize / 1024 / 1024 > 1:
+	if logsize / 1024 / 1024 > 2:
 		os.remove("../result/slidingpeak.log")
-	logging.basicConfig(level = logging.INFO, 
-						format = "%(asctime)s %(filename)s line:%(lineno)d %(levelname)s\n %(message)s",
-						datefmt = "%b %d %Y %H:%M:%S",
-						filename = "../result/slidingpeak.log",
-						filemode = "a")
+	# set logging configure
+	FORMAT = "%(asctime)s %(filename)s line:%(lineno)d %(levelname)s\n %(message)s"
+	FNM = "../result/slidingpeak.log"
+	DATEFMT = "%Y-%m-%d %H:%M:%S";
+	logging.basicConfig(level = logging.INFO, format = FORMAT,datefmt = DATEFMT,filename = FNM,filemode = "a")
 	console = logging.StreamHandler()
 	console.setLevel(logging.INFO)
 	console.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
 	logging.getLogger("").addHandler(console)
 	# present algorithm
-	logging.info("Affinity Propagation") # comment on log
-	for winsize in [4,8,12,16]:
-		logging.info(str(winsize))
+	#logging.info("3 sigma principle, fft") # comment on log
+	for winsize in [4]:
+		#logging.info("Window size: " + str(winsize))
 		eval = slidingpeak(winsize)
 		logging.info(str(eval)) # output accuracy matrix
-	#"""
-	#outputwindow(3)
+	"""
+	eval = nowin()
+	logging.info(str(eval))
+	outputwindow(3)
+	"""
