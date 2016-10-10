5bf58e74f6ba779092c0ac02526cf417061d3628
yszhong <15yszhong@tongji.edu.cn>
2016-10-10 17:46:53 +0800

    icss algorithm finish

--- pythonsrc/icss.py
+++ pythonsrc/icss.py
@@ -18,33 +18,37 @@ def cumsum(data):
 	return data
 
 def centercusum(data):
+	dat = range(len(data))
 	for i in range(len(data)):
-		data[i] = data[i] * data[i]
-	ck = cumsum(data)
-	ct = ck[len(ck) - 1]
+		dat[i] = math.pow(data[i], 2)
+	ck = cumsum(dat)
+	ct = ck[-1]
 	t = len(data)
 	dk = numpy.zeros(len(ck))
 	for i in range(len(ck)):
-		dk[i] = ck[i] / ct - i / t
+		dk[i] = float(ck[i]) / ct - float(i + 1) / t
+	#import pdb
+	#pdb.set_trace()
 	return dk
 
-def step(data):
+def step(dat, B):
+	data = dat
 	changepoints = []
 	dk = centercusum(data)
-	e, pos1 = checkcritical(dk)
+	e, pos1 = checkcritical(dk, B)
 	if e:
-		p=pos1
+		p = pos1
 		while e:
 			t2 = p
 			dk2a = centercusum(data[:t2 + 1])
-			e, p = checkcritical(dk2a)
+			e, p = checkcritical(dk2a, B)
 		kfirst = t2
 		p = pos1 + 1
 		e = True
 		while e:
 			t1 = p
 			dk2b = centercusum(data[t1:])
-			e, p2 = checkcritical(dk2b)
+			e, p2 = checkcritical(dk2b, B)
 			p = p2 + p
 		klast = t1 - 1
 		if kfirst == klast:
@@ -53,15 +57,15 @@ def step(data):
 			changepoints = [kfirst, klast]
 	return changepoints
 
-def checkcritical(dk):
-	Dstar = 1.358
+def checkcritical(dk, B):
+	Dstar = {0.5:0.828, 0.75:1.019, 0.90:1.224, 0.95:1.358, 0.99:1.628}
 	absdk = dk
 	for i in range(len(absdk)):
 		absdk[i] = abs(absdk[i])
 	value = max(absdk)
 	position = numpy.argmax(absdk)
 	M = value * math.sqrt(len(absdk) / 2)
-	if M > Dstar:
+	if M > Dstar[B]:
 		exceed = True
 	else:
 		exceed = False
@@ -81,29 +85,34 @@ def isconverged(old, new):
 		converged = False
 	return converged
 
-def ICSS(data):
-	potential = step(data)
-	potential.insert(0, 0)
-	potential.append(len(data))
+def ICSS(dat, B = 0.95):
+	data = dat
+	potential = step(data, B)
+	potential.append(len(data) - 1)
+	potential.append(0)
 	potential = list(set(potential))
+	potential.sort()
 	converged = False
 	while not converged:
 		newcps = []
-		for i in range(1, len(potential) - 2):
+		for i in range(1, len(potential) - 1):
 			frm = potential[i - 1] + 1
 			to = potential[i + 1]
 			Dk = centercusum(data[frm:to + 1])
-			e, p = checkcritical(Dk)
+			e, p = checkcritical(Dk, B)
 			if e:
 				newcps.append(frm + p)
-		newcps.insert(0, 0)
-		newcps.append(len(data))
+		newcps.append(0)
+		newcps.append(len(data) - 1)
+		newcps = list(set(newcps))
+		newcps.sort()
 		converged = isconverged(potential, newcps)
 		if not converged:
 			potential = newcps
-	changepoints = potential[1:len(potential)]
+	changepoints = potential[1:len(potential) - 1]
 	return changepoints
 
 if __name__ == "__main__":
-	data = [1, 2, 3]
+	data = [3, 2, 17, 4, 3, 6, 806, 2, 7, 9, 11, 22, 1, 0, 0, 4, 3, 5]
 	changepoints = ICSS(data)
+	print changepoints
